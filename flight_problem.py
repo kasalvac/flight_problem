d9dda0766fbf169194f41a635d8cfbe3b245d09c
kasalvac <kasal.emailer@gmail.com>
2024-08-02 15:43:42 +0200

    find_possible_destinations

--- flight_problem.py
+++ flight_problem.py
@@ -1,28 +1,39 @@
 airports = [
 "BGI", "CDG", "DEL", "DOH", "DSM", "EWR", "EYW", "HND", "ICN",
 "JFK", "LGA", "LHR", "ORD", "SAN", "SFO", "SIN", "TLV", "BUD",
+]
 
-1
-
-routes =[
-
-['DSM", "ORD"],
-['ORD", "BGI"],
-['BGI", "LGA"]
-['SIN", "CDG"],
-['CDG", "SIN"],
-['CDG", "BUD"],
-['DEL", "DOH"],
-['DEL", "CDG"],
-['TLV", "DEL"],
-['EWR", "HND"],
+routes = [
+["DSM", "ORD"],
+["ORD", "BGI"],
+["BGI", "LGA"],
+["SIN", "CDG"],
+["CDG", "SIN"],
+["CDG", "BUD"],
+["DEL", "DOH"],
+["DEL", "CDG"],
+["TLV", "DEL"],
+["EWR", "HND"],
 ["HND", "ICN"],
 ["HND", "JFK"],
 ["ICN", "JFK"],
-['JFK", "LGA"],
+["JFK", "LGA"],
 ["EYW", "LHR"],
-['LHR", "SFO"],
-['SFO", "SAN"],
-['SFO", "DSM"],
+["LHR", "SFO"],
+["SFO", "SAN"],
+["SFO", "DSM"],
 ["SAN", "EYW"],
+]
 
+starting_airport = ["LGA"]
+
+def find_possible_destinations(start):
+    options = []
+    for flight in routes:
+        if flight[0] == start:
+            options.append(flight[1])
+
+    return (options)
+
+test = find_possible_destinations("CDG")
+print(test)
